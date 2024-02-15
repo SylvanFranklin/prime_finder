9d1bab5d0f808d83e690c197fd60f5b074c1b057
Sylvan Franklin <sylvanfranklin@icloud.com>
2024-02-15 10:02:21 -0500

    primes.py

--- primes.py
+++ primes.py
@@ -1,47 +1,35 @@
-""" Rosetta Code task rosettacode.org/wiki/Sieve_of_Pritchard """
+# Start with a set W={1} and length=1 representing wheel 0, and prime p=2.
+# As long as p2 <= N, do the following
+#     if length < N then
+#         extend W by repeatedly getting successive members w of W starting with 1 and inserting length+w into W as long as it doesn't exceed p*length or N;
+#         increase length to the minimum of p*length and N.
+#     repeatedly delete p times each member of W by first finding the largest <= length and then working backwards.
+#     note the prime p, then set p to the next member of W after 1 (or 3 if p was 2).
+# if length < N then extend W to N by repeatedly getting successive members w of W starting with 1 and inserting length+w into W as long as it doesn't exceed N;
+# On termination, the rest of the primes up to N are the members of W after 1.
 
-from numpy import ndarray
-from math import isqrt
+def pritchard(n):
+    w = [1]
+    length = 1
+    primes = [2, 3]
+    p = 2
 
+    while p ** 2 <= n:
+        # make an initial wheel
+        if length < n:
+            for i in w:
+                j = i + length 
+                if j < p * length and p < n:
+                    w += j
 
-def pritchard(limit):
-    """ Pritchard sieve of primes up to limit """
-    members = ndarray(limit + 1, dtype=bool)
-    members.fill(False)
-    members[1] = True
-    steplength, prime, rtlim, nlimit = 1, 2, isqrt(limit), 2
-    primes = []
-    while prime <= rtlim:
-        if steplength < limit:
-            for w in range(1, len(members)):
-                if members[w]:
-                    n = w + steplength
-                    while n <= nlimit:
-                        members[n] = True
-                        n += steplength
-            steplength = nlimit
+            length = min(p*length, n)
+        # remove the duplicates
+        for i in range(len(wheel)):
+            if wheel[i] % p == 0:
+                wheel.pop(i)
 
-        np = 5
-        mcpy = members.copy()
-        for w in range(1, len(members)):
-            if mcpy[w]:
-                if np == 5 and w > prime:
-                    np = w
-                n = prime * w
-                if n > nlimit:
-                    break  # no use trying to remove items that can't even be there
-                members[n] = False  # no checking necessary now
+        # go to the next prime
+        primes.append(p)
+        p = w[1]
 
-        if np < prime:
-            break
-        primes.append(prime)
-        prime = 3 if prime == 2 else np
-        nlimit = min(steplength * prime, limit)  # advance wheel limit
-
-    newprimes = [i for i in range(2, len(members)) if members[i]]
-    return sorted(primes + newprimes)
-
-
-print(pritchard(150))
-print('Number of primes up to 1,000,000:', len(pritchard(1000000)))
-
+    return primes
